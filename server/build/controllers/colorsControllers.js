95d22808d3b20824ae7dc0ce404063d82acae846
ArtyomBobr <artyombobr@gmail.com>
2019-04-15 18:34:19 +0300

    Исправил package

---
+++ server/build/controllers/colorsControllers.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Colors_1 = __importDefault(require("../helpers/Colors"));
+const shri_json_1 = __importDefault(require("./../shri.json"));
+const colors = Colors_1.default.factory(shri_json_1.default.colors);
+exports.getColors = (req, res) => {
+    res.status(200).json(colors.toArray());
+};
+//# sourceMappingURL=colorsControllers.js.map
